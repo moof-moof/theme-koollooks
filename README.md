b58fa7de40f8f99d2721dec92eccdd284bb8f187
Loxia labs <bergman.martin@gmail.com>
2026-06-12 13:47:40 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# theme-koollooks
-Koollooks is a Tkinter theme á la classic Macintosh System 6 Finder.
+
+# Koollooks ttk theme
+
+
+
+### Description
+Koollooks is a Tkinter theme designed to somewhat emulate the classic Macintosh System 6 GUI, i.e. the original "Finder" desktop environment.
+
+It is based on the venerable Clearlooks theme, which in turn builds on the Clam theme.
+
+Included in this repository is a simple Demo app, which has no actual function other than showcasing the theme's graphical characteristics. See screenshots below.
+
+On a linux system the tcl-files provided here would typically be located in ~/.local/share/tcltk.
+
+The inherited license is Tcl  <https://www.tcl-lang.org/software/tcltk/license.html>
+
+![ ](./meta/Finder_demo_18.png)
+![ ](./meta/Finder_demo_18_(menu).png)
---
+++ ttk-widgets-demo_v18.py
@@ -0,0 +1,293 @@
+#!/usr/bin/python3
+"""
+LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL
+
+    Dummy demonstrator for the Classic Mac inspired
+    ttk theme "Koollooks" by Dogcow (moof-moof) 2026.
+    
+    The demo script itself is based on 
+    "ttk-example.py" by RedFantom 2018.
+    License: GNU GPLv3
+    
+LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL
+"""
+
+import os
+import tkinter as tk
+from tkinter import ttk
+from tkinter import Tk
+from tkinter import font
+from ttkthemes import ThemedTk, THEMES
+from ttkwidgets import ScaleEntry
+from ttkwidgets.autocomplete import AutocompleteCombobox
+from PIL import Image, ImageTk, ImageDraw
+
+
+# Version number
+vnum ='v18'
+
+# WIDTH, HEIGHT = 500, 700
+
+# Chicago text "colours"
+chi_fg =     '#000000'
+chi_bg =     '#ffffff'
+chi_act_fg = '#ffffff'
+chi_act_bg = '#000000'
+
+
+
+def main():
+    app = Example()
+    app.mainloop()
+
+        
+
+class Example(ThemedTk):
+    
+
+    def __init__(self, theme="koollooks"):
+        
+        ThemedTk.__init__(self, fonts=True, themebg=True)
+        self.set_theme(theme)
+        self.title('Finder Demo '+ vnum)
+        self.resizable(True, True)
+
+        self.style = ttk.Style()
+        self.style.theme_use(theme)
+        
+    # Some global customizations
+        self.style.map("TButton", foreground=[('pressed', chi_bg)])
+        self.buttonFont = ('Chicago Kare', 12)
+        self.monofont = ('monaco-12-(accurate)', 12)
+        
+        self.img_indicator = tk.PhotoImage(file=os.path.expanduser( \
+                                                     "~/koollooks_alias/sub-menu-indicator-sn.gif"))        
+    # Preload our 36x28 pixel fancy button images
+        self.img_active  = tk.PhotoImage(file=os.path.expanduser("~/koollooks_alias/button-ax.gif"))
+        self.img_disabld = tk.PhotoImage(file=os.path.expanduser("~/koollooks_alias/button-dx.gif"))
+        self.img_normal  = tk.PhotoImage(file=os.path.expanduser("~/koollooks_alias/button-nx.gif"))
+        self.img_pressed = tk.PhotoImage(file=os.path.expanduser("~/koollooks_alias/button-px.gif"))
+        
+    # Let's turn off every kind of focus-indication already!
+        self.style.configure("TNotebook.Tab", focuscolor="chi_bg")
+        self.option_add('*TNotebook*takeFocus',    0)
+        self.option_add('*TButton*takeFocus',      0)
+        self.option_add('*TRadiobutton*takeFocus', 0)
+        self.option_add('*TCheckbutton*takeFocus', 0)
+
+
+
+#  Create widgets:
+#  ***************
+
+    # Menubar
+        self.setup_menubar()
+    # Notebook
+        self.notebook = ttk.Notebook(self)
+        self.notebook.add(ttk.Button(self, style="nobo.TButton", text="Akchully...", width=2), text="Tab 1" )
+        self.notebook.add(ttk.Button(self, style="nobo.TButton", text="Hello Universe"), text="Tab 2")
+    # Label        
+        self.labelv = ttk.Label(self, text='  ['+ vnum + ']  ')   
+    # Options menu        
+        self.dropdown = ttk.OptionMenu(self, tk.StringVar(), " Pick-val", "1st Value", "2nd Value")
+        self.dropdown["menu"].config(background=chi_bg,
+                            activebackground=chi_act_bg,
+                            activeforeground=chi_act_fg)
+    # TextEntry box        
+        self.entry = ttk.Entry(self, textvariable=tk.StringVar(value="Default entry value."))
+    # Button(Fancy)
+        self.button1 = self.fancy_butt("OK", 5, self.do_nuthin()) 
+    # Button(Plain)
+        self.button2 = ttk.Button(self, style="Plain.TButton", width=6, padding=5, text="Cancel") 
+    # Radio        
+        self.radio_one = ttk.Radiobutton(self, text="Off", value=True)
+        self.radio_two = ttk.Radiobutton(self, text="On", value=False)
+    # Listbox
+        self.listbox = tk.Listbox(self, width=3, height=9, font=self.monofont, bg=chi_bg,
+                    selectbackground=chi_act_bg, selectforeground=chi_act_fg, activestyle='none')
+        self.listbox.insert(tk.END, *(f" #{i}" for i in range(100)))
+    # Scrollbar
+        self.scrollv = ttk.Scrollbar(self, orient=tk.VERTICAL, command=self.listbox.yview) 
+        self.listbox.configure(yscrollcommand=self.scrollv.set)
+        #self.listbox['yscrollcommand']=self.scrollv.set   # similar alternative statement
+    # Checkbtn     
+        self.checked = ttk.Checkbutton(self, text="Checked", variable=tk.BooleanVar(value=True))
+        self.unchecked = ttk.Checkbutton(self, text="Unchecked")
+    # Treeview
+        self.tree = ttk.Treeview(self, height=5, show=("tree", "headings"))
+        self.setup_tree()
+    # ScaleEntry
+        self.scale_entry = ScaleEntry(self, from_=0, to=50, orient=tk.HORIZONTAL, compound=tk.RIGHT)
+    # Combobox
+        self.style.configure('TCombobox', background=chi_bg, foreground=chi_fg)
+        self.combo = AutocompleteCombobox(self, completevalues=["something", "nothing", "Tralah"])
+        self.option_add("*TCombobox*Listbox*Background", chi_bg)
+        self.option_add("*TCombobox*Listbox*Foreground", chi_fg)
+    # Progress bar
+        self.progress = ttk.Progressbar(self, maximum=100, value=70)
+    # Sizegrip
+        self.sizegrip = ttk.Sizegrip(self)
+    # Separator (horisontal) as spacer?:
+        self.separator1h = ttk.Separator()
+
+
+#  Do da layout:
+#  *************   
+    
+    # Grid widgets
+        self.grid_widgets()
+
+
+     
+    def do_nuthin(self):
+        pass
+
+    def change_title(self):
+        self.title("OK )Yahooo(")
+        
+    def setup_menubar(self):
+        """Setup a standard menubar"""
+        self.menubar = tk.Menu(self,
+                            bg=chi_bg,
+                            activebackground=chi_act_bg,
+                            activeforeground=chi_act_fg,
+                            borderwidth=1)
+        self.config(menu=self.menubar)
+    # Apple -----------------------------------------------        
+        self.apple_menu = tk.Menu(self.menubar,
+                            bg=chi_bg,
+                            tearoff=False,
+                            activebackground=chi_act_bg,
+                            activeforeground=chi_act_fg)
+        self.apple_menu.add_command(label="About...", command=self.do_nuthin())
+        self.apple_menu.add_command(label="Help", command=self.do_nuthin())
+        self.menubar.add_cascade(menu=self.apple_menu, \
+                                label=(u'\uf8ff'), \
+                                font=('Chicago Kare', 12)) 
+    # File -----------------------------------------------
+        self.file_menu = tk.Menu(self.menubar,
+                            bg=chi_bg,
+                            tearoff=False,
+                            activebackground=chi_act_bg,
+                            activeforeground=chi_act_fg)
+        self.file_menu.add_command(label="New", command=self.do_nuthin())
+        self.file_menu.add_command(label="Open", command=self.do_nuthin())
+        self.file_menu.add_command(label="Save", command=self.do_nuthin(), state="disabled")
+        self.file_menu.add_command(label="Save as...", command=self.do_nuthin())
+        self.file_menu.add_separator()
+        self.file_menu.add_command(label="Quit", command=self.destroy)
+        self.menubar.add_cascade(menu=self.file_menu, label="File")
+    # Edit -----------------------------------------------
+        self.edit_menu = tk.Menu(self.menubar,
+                            bg=chi_bg,
+                            tearoff=False,
+                            activebackground=chi_act_bg,
+                            activeforeground=chi_act_fg)
+        self.edit_menu.add_command(label="Cut", command=self.do_nuthin())
+        self.edit_menu.add_command(label="Copy", command=self.do_nuthin())
+        self.edit_menu.add_command(label="Paste", command=self.do_nuthin())
+        self.edit_menu.add_command(label="Delete", command=self.do_nuthin())
+        self.edit_menu.add_command(label="Select All", command=self.do_nuthin())
+    # Edit>Options ----------------------------------------
+        self.sub_edit_menu = tk.Menu(self.edit_menu,
+                            bg=chi_bg,
+                            tearoff=False,
+                            relief="flat",  # Doesn't kill the corny relief arrow ...
+                            activebackground=chi_act_bg,
+                            activeforeground=chi_act_fg)
+        self.sub_edit_menu.add_command(label="Subadub", command=self.do_nuthin())
+        self.sub_edit_menu.add_command(label="Yada", command=self.do_nuthin())
+        self.sub_edit_menu.add_command(label="Nada", command=self.do_nuthin())
+        self.edit_menu.add_cascade( menu=self.sub_edit_menu, 
+                                    label="Options ",
+                                    bitmap="", 
+                                    image=self.img_indicator, 
+                                    compound='right')
+        self.menubar.add_cascade(menu=self.edit_menu, label="Edit")
+
+    def setup_tree(self):
+        """Setup an example Treeview"""
+        self.tree.insert("", tk.END, text="Example 1", iid="1", open=False)
+        self.tree.insert("", tk.END, text="Example 2", iid="2")
+        self.tree.insert('1', tk.END, text='Exmpl 11', iid="11")
+        self.tree.insert('1', tk.END, text='Exmpl 12', iid="12",open=False)
+        self.tree.insert("12", tk.END, text='exmpl 121', iid="121")
+        
+        self.tree.heading("#0", text="Tree Heading")
+
+    def fancy_butt(self, txt, hpad, cmd):        
+        ''' 
+    1. Map the 7-pixel border regions '''
+        # format: border = (left, top, right, bottom)
+        # Setting top/bottom to 0 prevents vertical scaling distortions
+        self.style.element_create(
+            "Custom.Button.background", 
+            "image", 
+            self.img_normal,
+            ("pressed", self.img_pressed),
+            ("active", self.img_active),
+            border=(7, 0, 7, 0),    # Protects 7px on each side (left/right)
+            sticky="ew"             # Restricts stretching strictly to the X-axis
+        )
+        ''' 
+    2. Rebuild button layout with centered text '''
+        self.style.layout(
+            "Custom.TButton",
+            [
+                ("Custom.Button.background", {"children": [
+                    ("Button.padding", {"children": [ 
+                        ("Button.label", {"sticky": "nswe"}) 
+                    ], 'sticky': 'nswe'})
+                ], 'sticky': 'nswe'})
+            ]
+        )
+        '''
+    3. Enforce the fixed 28px height '''
+        # Koolooks/clam may add default padding, so we control that explicitly here.
+        # Inside padding: left/right, top/bottom (managed by image height).
+        self.style.configure("Custom.TButton", padding=((hpad-12), 0))
+        # Force the button widget to match our 28px image limit natively.
+        self.option_add("*Custom.TButton.height", 28)
+        # This width param is ignored, just stuck on 78px (7+64+7) even with padding=0!
+        self.option_add("*Custom.TButton.width", 0)
+
+        return ttk.Button(self, style="Custom.TButton", text=txt, command=cmd)
+  
+    
+    def grid_widgets(self):
+        """Put widgets in the grid"""
+        sticko = {"sticky": "news"}
+        self.notebook.grid(row=0, column=1, columnspan=2, **sticko)
+        self.labelv.grid(row=9, column=0, columnspan=1, sticky="ew")
+        self.dropdown.grid(row=2, column=1, **sticko)
+        self.entry.grid(row=2, column=2, **sticko)
+        self.button1.grid(row=3, column=0, columnspan=1, padx=10, pady=10)
+        self.button2.grid(row=3, column=1, columnspan=1, sticky=tk.NS)
+        self.radio_two.grid(row=4, column=1, **sticko)
+        self.radio_one.grid(row=4, column=2, **sticko)
+        self.checked.grid(row=5, column=1, **sticko)
+        self.unchecked.grid(row=5, column=2, **sticko)        
+        self.listbox.grid(row=6, column=0, rowspan=3, padx=12, pady=0, sticky="new")
+        self.scrollv.grid(row=0, column=3, rowspan=9, padx=5, pady=5, **sticko)
+        self.tree.grid(row=6, column=1, columnspan=2, **sticko)
+        self.scale_entry.grid(row=7, column=1, columnspan=2, **sticko)
+        self.combo.grid(row=8, column=1, columnspan=2, sticky="w")
+        self.progress.grid(row=9, column=1, columnspan=2, padx=5, pady=5, **sticko)
+        self.sizegrip.grid(row=9, column=3 )
+        self.separator1h.grid(row=10, column=1, sticky="we")
+
+
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
+
+
